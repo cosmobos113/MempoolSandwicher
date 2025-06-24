5b19c5916ec94a5e0ee85b2254c64eface2624cd
cosmobos113 <123627414+cosmobos113@users.noreply.github.com>
2025-06-24 14:38:01 +0300

    Create flashbots.ts

---
+++ src/flashbots.ts
@@ -0,0 +1,25 @@
+import { ethers } from 'ethers';
+import { FlashbotsBundleProvider } from '@flashbots/ethers-provider-bundle';
+
+export async function submitBundle(
+  txs: Array<ethers.TransactionRequest>,
+  gasPrice: ethers.BigNumberish,
+  provider: ethers.JsonRpcProvider
+) {
+  const authSigner = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+  const flashbotsProvider = await FlashbotsBundleProvider.create(
+    provider,
+    authSigner,
+    process.env.FLASHBOTS_RELAY!
+  );
+
+  const signedBundle = await flashbotsProvider.signBundle(
+    txs.map((tx) => ({ transaction: tx, signer: authSigner }))
+  );
+
+  const blockNumber = await provider.getBlockNumber();
+  const bundleResponse = await flashbotsProvider.sendRawBundle(signedBundle, blockNumber + 1);
+
+  console.log('Bundle submitted for block', blockNumber + 1);
+  return bundleResponse;
+}
