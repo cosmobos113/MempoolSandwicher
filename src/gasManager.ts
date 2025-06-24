a0eda973adc3151c225e0ba4b8184e94bb35b69c
cosmobos113 <123627414+cosmobos113@users.noreply.github.com>
2025-06-24 17:44:19 +0300

    Update gasManager.ts

--- src/gasManager.ts
+++ src/gasManager.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers';
 
 export async function estimateGasPrice(provider: ethers.Provider): Promise<ethers.BigNumber> {
-
   const feeData = await provider.getFeeData();
   return feeData.maxFeePerGas!;
 }
